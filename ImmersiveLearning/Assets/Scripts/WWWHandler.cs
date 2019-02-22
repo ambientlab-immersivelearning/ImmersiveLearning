11fce1e662340ee92d14eaa9b2f80ceac1f1a8cf
jsome635 <jsome635@hawaii.edu>
2019-02-21 22:28:00 -1000

    Commit for branch switch.

---
+++ ImmersiveLearning/Assets/Scripts/WWWHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class WWWHandler : MonoBehaviour {
+    public string ObjectUrl = "https://raw.githubusercontent.com/ambientlab-immersivelearning/ImmersiveLearning/objects/ImmersiveLearning/Assets/Prefabs/Cube.prefab";
+
+    public GameObject temp;
+    
+    IEnumerator Start() {
+        using (UnityWebRequest www = UnityWebRequest.Get(ObjectUrl)) {
+            yield return www;
+            Instantiate(www);
+        }
+    }
+}
