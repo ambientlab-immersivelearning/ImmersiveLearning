15aa60cfcb2a81e882538e37701874a407a1af35
g-tolentino <gmtolent@hawaii.edu>
2019-04-16 20:32:11 -1000

    Commit to switch branch

---
+++ ImmersiveLearning/Assets/Scripts/BoundingBox.cs
@@ -0,0 +1,29 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundingBox : MonoBehaviour, IFocusable
+{
+    public void OnFocusEnter()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnFocusExit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ImmersiveLearning/Assets/Scripts/NRSRManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity;
+
+public class NRSRManager : Singleton<NRSRManager>
+{
+    public Renderer[] ObjectsInScene;
+    public List<GameObject> FilterObjectsInScene = new List<GameObject>();
+
+    //Used to trigger next object update
+    public int TotalNumberOfObjects = 0;
+    public int PreviousFrameObjectCount = 0;
+
+    //Just incase we need - numbers added should equal total number of objects
+    public int numberofVisibleObjects;
+    public int numberOfFilteredObjects;
+
+    private void FixedUpdate()
+    {
+        FindObjectsInScene();
+
+        TotalNumberOfObjects = ObjectsInScene.Length;
+
+        if (TotalNumberOfObjects != PreviousFrameObjectCount)
+        {
+            FilterUnneededObjects();
+            numberofVisibleObjects = FilterObjectsInScene.Count;
+
+            foreach (GameObject go in FilterObjectsInScene)
+            {
+
+            }
+        }
+
+        PreviousFrameObjectCount = ObjectsInScene.Length;
+    }
+
+    private void FindObjectsInScene()
+    {
+        ObjectsInScene = null;
+        ObjectsInScene = FindObjectsOfType<Renderer>();
+    }
+
+    private void FilterUnneededObjects()
+    {
+        FilterObjectsInScene.Clear();
+        numberOfFilteredObjects = 0;
+
+        for (int i = 0; i < ObjectsInScene.Length; i++)
+        {
+            if (ObjectsInScene[i].gameObject.tag != "NRSRTools")
+            {
+                FilterObjectsInScene.Add(ObjectsInScene[i].gameObject);
+            }
+            else
+            {
+                numberOfFilteredObjects++;
+            }
+        }
+    }
+}
