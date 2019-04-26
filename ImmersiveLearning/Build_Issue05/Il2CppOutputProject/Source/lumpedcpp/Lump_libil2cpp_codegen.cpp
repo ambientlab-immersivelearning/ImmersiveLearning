b22d7985ebf53cbc461ef4eaa354ea5e274c79d9
jsome635 <jsome635@hawaii.edu>
2019-04-25 21:13:47 -1000

    Remove build folder.

--- ImmersiveLearning/Build_Issue05/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_codegen.cpp
+++
@@ -1,3 +0,0 @@
-#include "il2cpp-config.h"
-#include <C:\Users\gmros_000\Documents\GitHub\ImmersiveLearning\ImmersiveLearning\Build_Issue05\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-common.cpp>
-#include <C:\Users\gmros_000\Documents\GitHub\ImmersiveLearning\ImmersiveLearning\Build_Issue05\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-il2cpp.cpp>
