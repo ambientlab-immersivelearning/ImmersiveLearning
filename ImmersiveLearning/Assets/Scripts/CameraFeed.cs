14650594b616fd4333388c90ce7336151fe6cae0
g-tolentino <gmtolent@hawaii.edu>
2019-04-29 19:42:32 -1000

    Scanner reads and shows picture output

--- ImmersiveLearning/Assets/Scripts/CameraFeed.cs
+++ ImmersiveLearning/Assets/Scripts/CameraFeed.cs
@@ -2,11 +2,17 @@
 using System.Collections;
 using System.Linq;
 using UnityEngine.XR.WSA.WebCam;
+using ZXing;
+using UnityEngine.UI;
 
 public class CameraFeed : MonoBehaviour
 {
+    public Text statusText;
+
     PhotoCapture photoCaptureObject = null;
     Texture2D targetTexture = null;
+    GameObject quad = null;
+    Renderer quadRenderer;
 
     // Use this for initialization
     void Start()
@@ -14,8 +20,18 @@ public class CameraFeed : MonoBehaviour
         Resolution cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
         targetTexture = new Texture2D(cameraResolution.width, cameraResolution.height);
 
+        // Create a GameObject to which the texture can be applied
+        quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        quadRenderer = quad.GetComponent<Renderer>() as Renderer;
+        quadRenderer.material = new Material(Shader.Find("Unlit/Texture"));
+
+        quad.transform.parent = this.transform;
+        quad.transform.position = new Vector3(-0.6f, -0.3f, 3.0f);
+        quad.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+
         // Create a PhotoCapture object
-        PhotoCapture.CreateAsync(false, delegate (PhotoCapture captureObject) {
+        PhotoCapture.CreateAsync(false, delegate (PhotoCapture captureObject)
+        {
             photoCaptureObject = captureObject;
             CameraParameters cameraParameters = new CameraParameters();
             cameraParameters.hologramOpacity = 0.0f;
@@ -24,7 +40,8 @@ public class CameraFeed : MonoBehaviour
             cameraParameters.pixelFormat = CapturePixelFormat.BGRA32;
 
             // Activate the camera
-            photoCaptureObject.StartPhotoModeAsync(cameraParameters, delegate (PhotoCapture.PhotoCaptureResult result) {
+            photoCaptureObject.StartPhotoModeAsync(cameraParameters, delegate (PhotoCapture.PhotoCaptureResult result)
+            {
                 // Take a picture
                 photoCaptureObject.TakePhotoAsync(OnCapturedPhotoToMemory);
             });
@@ -35,19 +52,29 @@ public class CameraFeed : MonoBehaviour
     {
         // Copy the raw image data into the target texture
         photoCaptureFrame.UploadImageDataToTexture(targetTexture);
-
-        // Create a GameObject to which the texture can be applied
-        GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
-        Renderer quadRenderer = quad.GetComponent<Renderer>() as Renderer;
-        quadRenderer.material = new Material(Shader.Find("Custom/Unlit/UnlitTexture"));
-
-        quad.transform.parent = this.transform;
-        quad.transform.localPosition = new Vector3(0.0f, 0.0f, 3.0f);
-
         quadRenderer.material.SetTexture("_MainTex", targetTexture);
 
-        // Deactivate the camera
-        photoCaptureObject.StopPhotoModeAsync(OnStoppedPhotoMode);
+        // Decode Camera Texture
+        IBarcodeReader barcodeReader = new BarcodeReader();
+        Result qrCode = barcodeReader.Decode(targetTexture.GetPixels32(), targetTexture.width, targetTexture.height);
+
+        // Check if QR code was seen
+        if (qrCode != null)
+        {
+            string qrResult = qrCode.Text;
+            Debug.Log("DECODED TEXT: " + qrResult);
+            statusText.text = qrResult;
+
+            // Deactivate the camera
+            photoCaptureObject.StopPhotoModeAsync(OnStoppedPhotoMode);
+        }
+        else
+        {
+            Debug.Log("QR code not detected");
+            statusText.text = "Scanning for QR code...";
+            // Take another picture
+            photoCaptureObject.TakePhotoAsync(OnCapturedPhotoToMemory);
+        }
     }
 
     void OnStoppedPhotoMode(PhotoCapture.PhotoCaptureResult result)
